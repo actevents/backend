2a93f9775f2330b4cee3fe6658a58638d0d9735c
JSchmiegel <jonas@ullr>
2021-05-08 10:02:41 +0200

    Update to use /events/{id}

--- getEventID/lambda_function.py
+++ getEventID/lambda_function.py
@@ -3,36 +3,36 @@ import boto3
 
 #getEventID
 def lambda_handler(event, context):
-    #get parameters out of queryStringParameters
-    parameters = event["queryStringParameters"]
+    #get parameters out of pathParameters
+    parameters = event["pathParameters"]
 
-    dynamodb = boto3.resource('dynamodb')
-    table = dynamodb.Table('Events')
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table("Events")
     try:
-        id = str(parameters['id'])
-    except:
+        id = str(parameters["id"])
+    except Exception as ex:
         return {
             "statusCode": 400,
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
             },
-            "body": json.dumps("Error: Could not get id as parameter.")
+            "body": json.dumps("Error: Could not get id as parameter.\n" + str(type(ex)) + "\n\n" + str(ex.args))
         }
     try:
-        response = table.get_item(Key={'id':id})
-        item = response['Item']
+        response = table.get_item(Key={"id":id})
+        item = response["Item"]
         return {
-            'statusCode': 200,
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
             },
-            'body': json.dumps(item)
+            "body": json.dumps(item)
         }
-    except Exception as inst:
+    except Exception as ex:
         return {
-            'statusCode': 400,
-            'headers': {
-                'Access-Control-Allow-Origin': '*'
+            "statusCode": 400,
+            "headers": {
+                "Access-Control-Allow-Origin": "*"
             },
-            'body': json.dumps('Error: Error getting events by id\n' + str(type(inst)) + '\n\n' + str(inst.args))
+            "body": json.dumps("Error: Error getting events by id\n" + str(type(ex)) + "\n\n" + str(ex.args))
         }
