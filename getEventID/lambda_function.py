1a07e7ffe333547b3b714725667c5f5511768478
JSchmiegel <jonas@ullr>
2021-05-18 09:26:04 +0200

    updated getEventID to (optional) get the distance

--- getEventID/lambda_function.py
+++ getEventID/lambda_function.py
@@ -1,5 +1,6 @@
 import json
 import boto3
+from math import cos, asin, sqrt, pi
 
 #getEventID
 def lambda_handler(event, context):
@@ -8,18 +9,7 @@ def lambda_handler(event, context):
 
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("Events")
-    try:
-        user = event["requestContext"]["authorizer"]["claims"]
-        email = user["email"]
-    except Exception as inst:
-        print("")
-        return {
-            "statusCode": 400,
-            "headers": {
-                "Access-Control-Allow-Origin": "*"
-            },
-            "body": json.dumps("Error getting the user of request\n\n" + str(type(inst)) + "\n\n" + str(inst.args))
-        }
+    #try to get id of parameters
     try:
         id = str(parameters["id"])
     except Exception as ex:
@@ -30,17 +20,31 @@ def lambda_handler(event, context):
             },
             "body": json.dumps("Error: Could not get id as parameter.\n" + str(type(ex)) + "\n\n" + str(ex.args))
         }
+    #try to get location
+    try:
+        parameters = event["queryStringParameters"]
+        lon = float(parameters["longitude"])
+        lat = float(parameters["latitude"])
+        hasLocation = True
+    except KeyError:
+        hasLocation = False
+        print("no location given")
+    #try to get item
     try:
         response = table.get_item(Key={"id":id})
         item = response["Item"]
-        # if not item["organizer"] == email:
-        #     return {
-        #         "statusCode": 400,
-        #         "headers": {
-        #             "Access-Control-Allow-Origin": "*"
-        #         },
-        #         "body": "You are not the organizer of the event!"
-        #     }
+        if hasLocation:
+            try:
+                distance = getDistance(lat,lon,float(item["location"]["latitude"]),float(item["location"]["longitude"]))
+                item.update({"distance": distance})
+            except Exception as inst:
+                return {
+                    "statusCode": 400,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    "body": json.dumps("Error calculating the distance\n\n" + str(type(inst)) + "\n\n" + str(inst.args))
+                }
         return {
             "statusCode": 200,
             "headers": {
@@ -56,3 +60,8 @@ def lambda_handler(event, context):
             },
             "body": json.dumps("Error: Error getting events by id\n" + str(type(ex)) + "\n\n" + str(ex.args))
         }
+
+def getDistance(lat1, lon1, lat2, lon2):
+    p = pi/180
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
+    return 12742 * asin(sqrt(a)) #2*R*asin...
