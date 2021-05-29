e1871595cb88e420453631c2199b3666021b1f53
JSchmiegel <jonas@ullr>
2021-05-29 12:32:09 +0200

    updated lambda to return list of events not of ids

--- getFavorites/lambda_function.py
+++ getFavorites/lambda_function.py
@@ -5,23 +5,35 @@ def lambda_handler(event, context):
     #create connection for database
     dynamodb = boto3.resource("dynamodb")
     table = dynamodb.Table("Favorites")
+    tableEvents = dynamodb.Table("Events")
 
     #get user
     user = event["requestContext"]["authorizer"]["claims"]
     # user = {"email": "test@test.de"}
     userId = user["email"]
+    favorites = list()
 
     response = table.get_item(Key={"user": userId})
     if "Item" in response:
+        for item in response["Item"]["favorites"]:
+            try:
+                favorites.append(tableEvents.get_item(Key={"id":item})["Item"])
+            except Exception as ex:
+                return {
+                    "statusCode": 400,
+                    "headers": {
+                        "Access-Control-Allow-Origin": "*"
+                    },
+                    "body": json.dumps("Error getting the favorites from the db\n\n" + str(type(ex)) + "\n\n" + str(ex.args))
+                }
         return {
             'statusCode': 200,
             "headers": {
                 "Access-Control-Allow-Origin": "*"
             },
-            'body': json.dumps(response["Item"])
+            'body': json.dumps(favorites)
         }
     else:
-        favorites = list()
         item = {
             "user": userId,
             "favorites": favorites
